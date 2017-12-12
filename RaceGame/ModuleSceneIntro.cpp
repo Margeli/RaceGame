213f3cdd2ee4f088ea2bf6539444d2d4b2e8e15f
Normanbg <normanbg98@gmail.com>
2017-12-12 20:56:49 +0100

    Added more scenario

--- RaceGame/ModuleSceneIntro.cpp
+++ RaceGame/ModuleSceneIntro.cpp
@@ -31,8 +31,24 @@ bool ModuleSceneIntro::Start()
 	roads.add(road2);
 	
 	Cube road3(25, 1, 75);
-	road3.SetPos(152.5f , 0, 57.6f);
+	road3.SetPos(127.5f , 0, 37.5f);
 	roads.add(road3);
+
+	Cube road4(75, 1, 20);
+	road4.SetPos(102.5f, 0, -10);
+	roads.add(road4);
+
+	Cube road5(25, 1, 37.5f);
+	road5.SetPos(77.5f, 0, -38.75f);
+	roads.add(road5);
+
+	Cube road6(150, 1, 20);
+	road6.SetPos(140, 0, -67.5f);
+	roads.add(road6);
+
+	Cube road7(150, 1, 20);
+	road7.SetPos(167.5f, 0, -67.5f);
+	roads.add(road7);
 
 	for (p2List_item<Cube>* item = roads.getFirst(); item; item = item->next)
 	{
