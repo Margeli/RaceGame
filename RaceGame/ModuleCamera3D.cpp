e61ad947aa5c537771a7496e6d04a7c5a1636a1c
Normanbg <normanbg98@gmail.com>
2017-12-12 21:31:45 +0100

    Added the map (only missing the fork)
    Also added map

--- RaceGame/ModuleCamera3D.cpp
+++ RaceGame/ModuleCamera3D.cpp
@@ -15,7 +15,7 @@ ModuleCamera3D::ModuleCamera3D(Application* app, bool start_enabled) : Module(ap
 	Position = vec3(0.0f, 50.0f, 0.0f);
 	Reference = vec3(0.0f, 0.0f, 0.0f);
 
-	offset_to_player = vec3(0.0f, 8.0f, -15.0f);
+	offset_to_player = vec3(0.0f, 15.0f, -15.0f);
 }
 
 ModuleCamera3D::~ModuleCamera3D()
--- RaceGame/ModuleSceneIntro.cpp
+++ RaceGame/ModuleSceneIntro.cpp
@@ -22,39 +22,8 @@ bool ModuleSceneIntro::Start()
 	App->camera->Move(vec3(1.0f, 1.0f, 0.0f));
 	App->camera->LookAt(vec3(0, 0, 0));
 
-	Cube road1(20, 1, 150);
-	road1.SetPos(0, 0, 0);
-	roads.add(road1);
-
-	Cube road2(150, 1, 20);
-	road2.SetPos(65, 0, 85);
-	roads.add(road2);
+	StartTerrain();
 	
-	Cube road3(25, 1, 75);
-	road3.SetPos(127.5f , 0, 37.5f);
-	roads.add(road3);
-
-	Cube road4(75, 1, 20);
-	road4.SetPos(102.5f, 0, -10);
-	roads.add(road4);
-
-	Cube road5(25, 1, 37.5f);
-	road5.SetPos(77.5f, 0, -38.75f);
-	roads.add(road5);
-
-	Cube road6(150, 1, 20);
-	road6.SetPos(140, 0, -67.5f);
-	roads.add(road6);
-
-	Cube road7(150, 1, 20);
-	road7.SetPos(167.5f, 0, -67.5f);
-	roads.add(road7);
-
-	for (p2List_item<Cube>* item = roads.getFirst(); item; item = item->next)
-	{
-		App->physics->AddBody(item->data, 0);
-	}
-
 	return ret;
 }
 
@@ -85,3 +54,49 @@ void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
 {
 }
 
+void ModuleSceneIntro::StartTerrain()
+{
+	Cube road1(20, 15, 150);
+	road1.SetPos(0, 0, 0);
+	roads.add(road1);
+
+	Cube road2(150, 15, 20);
+	road2.SetPos(65, 0, 85);
+	roads.add(road2);
+
+	Cube road3(25, 15, 75);
+	road3.SetPos(127.5f, 0, 37.5f);
+	roads.add(road3);
+
+	Cube road4(75, 15, 20);
+	road4.SetPos(102.5f, 0, -10);
+	roads.add(road4);
+
+	Cube road5(25, 15, 37.5f);
+	road5.SetPos(77.5f, 0, -38.75f);
+	roads.add(road5);
+
+	Cube road6(150, 15, 20);
+	road6.SetPos(140, 0, -67.5f);
+	roads.add(road6);
+
+	Cube road7(20, 15, 37.5f);
+	road7.SetPos(205, 0, -96.25f);
+	roads.add(road7);
+
+	Cube road8(225, 15, 20);
+	road8.SetPos(102.5f, 0, -125);
+	roads.add(road8);
+
+	//Here we will put the divided road & we have to change the road8 to the medium length
+
+	Cube road12(20, 1, 40);
+	road12.SetPos(0, 0, -95);
+	roads.add(road12);
+
+	for (p2List_item<Cube>* item = roads.getFirst(); item; item = item->next)
+	{
+		App->physics->AddBody(item->data, 0);
+	}
+
+}
